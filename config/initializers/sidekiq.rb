8c5186bc7f7a41e2418a741cdfba68b98e268bce
niesamowityrycerz <lukas.winek11@gmail.com>
2021-05-20 09:58:44 +0200

    Configure redis and sidekiq for production

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,14 +1,24 @@
-
+# redis size == sidekiq concurrency
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379' }
+  config.redis = { 
+    size: 10,
+    network_timeout: 5,
+    url: ENV['REDIS_URL']
+  }
 end
 
+# it is recommended to keep +2 buffer value 
+# for Sidekiq redis config server value above
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379' }
+  config.redis = { 
+    size: 12,
+    network_timeout: 5,
+    url: ENV['REDIS_URL']
+  }
 end
 
 # do not run background process again
-# if it Sidekiq fails to deliver it 
+# if Sidekiq fails to deliver it 
 Sidekiq.default_worker_options = {
   retry: false
 }
