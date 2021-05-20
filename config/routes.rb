ceef7c355918f430e4d311f5ac0e3808e61c5a54
niesamowityrycerz <lukas.winek11@gmail.com>
2021-05-20 10:10:53 +0200

    check behavior on routes change

--- config/routes.rb
+++ config/routes.rb
@@ -15,13 +15,11 @@ Rails.application.routes.draw do
   mount Api::Root => '/'
   #mount GrapeSwaggerRails::Engine, at: '/documentation'
 
-  Rails.application.routes.draw do
-    Sidekiq::Web.use Rack::Auth::Basic do |username, password|
-      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV['SIDEKIQ_USERNAME'])) &
-        ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV['SIDEKIQ_PASSWORD']))
-    end
-    mount Sidekiq::Web, at: '/sidekiq'
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV['SIDEKIQ_USERNAME'])) &
+      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV['SIDEKIQ_PASSWORD']))
   end
+  mount Sidekiq::Web, at: '/sidekiq'
   
   mount RailsEventStore::Browser => '/res' if Rails.env.development?
 end
