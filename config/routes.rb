a3f7586b2c1c25b19d93dc1e9f01111a786efeec
niesamowityrycerz <lukas.winek11@gmail.com>
2021-05-20 23:08:40 +0200

    Small fix involving sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -15,12 +15,10 @@ Rails.application.routes.draw do
   mount Api::Root => '/'
   mount GrapeSwaggerRails::Engine, at: '/documentation'
 
-  if Rails.env == :production 
-    Sidekiq::Web.use Rack::Auth::Basic do |username, password|
-      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV['SIDEKIQ_USERNAME'])) &
-        ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV['SIDEKIQ_PASSWORD']))
-    end
-  end 
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV['SIDEKIQ_USERNAME'])) &
+      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV['SIDEKIQ_PASSWORD']))
+  end
   mount Sidekiq::Web, at: '/sidekiq'
   
   mount RailsEventStore::Browser => '/res' if Rails.env.development?
